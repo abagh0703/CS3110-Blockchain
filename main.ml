b371fe4127e5c131da44d4f619e0754d67e90c69
Aram Baghdassarian <abagh0703@gmail.com>
2018-05-07 22:31:30 -0400

    began post

--- main.ml
+++ main.ml
@@ -43,7 +43,23 @@ let chain_mux = Mutex.create ()
 let blkchn = ref BlockChain.empty
 let blkchn_mux = Mutex.create ()
 
+(* POST example: https://github.com/mirage/ocaml-cohttp *)
+(* open Lwt
+open Cohttp
+open Cohttp_lwt_unix
 
+let body =
+  Client.get (Uri.of_string "https://www.reddit.com/") >>= fun (resp, body) ->
+  let code = resp |> Response.status |> Code.code_of_status in
+  Printf.printf "Response code: %d\n" code;
+  Printf.printf "Headers: %s\n" (resp |> Response.headers |> Header.to_string);
+  body |> Cohttp_lwt.Body.to_string >|= fun body ->
+  Printf.printf "Body of length: %d\n" (String.length body);
+  body
+
+let () =
+  let body = Lwt_main.run body in
+  print_endline ("Received body\n" ^ body) *)
 
 type data = {
   mutable pub_key : string;
