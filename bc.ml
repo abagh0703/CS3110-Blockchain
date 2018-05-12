0c84d3dab7d4105b9fb8d823501bf2e6ce9602f4
Smedira <dts88@cornell.edu>
2018-05-12 12:41:35 -0400

    changed port

--- bc.ml
+++ bc.ml
@@ -5,7 +5,8 @@ open Mutex
 
 
 let get_value (url:string) (v:string ref) (m:Mutex.t) =
-  let _ = Client.get (Uri.of_string ("http://"^url)) >>>
+  let uri = Uri.make ~scheme:"http" ~host:url ~port:8081 () in
+  let _ = Client.get (uri) >>>
              (fun (r,b) ->
                upon (Cohttp_async.Body.to_string b)
                  (fun b ->
@@ -16,9 +17,9 @@ let get_value (url:string) (v:string ref) (m:Mutex.t) =
   Scheduler.go ()
 
 
-let post_value (url:string) (key:string) (value:string) host =
+let post_value (url:string) (key:string) (value:string) =
   let uri =
-    Uri.make ~scheme:"http" ~host:host ~port:8081 ~query:[key,[value]] () in
+    Uri.make ~scheme:"http" ~host:url ~port:8081 ~query:[key,[value]] () in
   let _ = Client.post (uri) >>> (fun _ -> shutdown 0) in
   Scheduler.go ()
   
