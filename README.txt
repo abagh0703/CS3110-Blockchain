7dab8159219e49eb57fafdd7e6dd8f24a10221f8
Smedira <dts88@cornell.edu>
2018-05-14 20:04:04 -0400

    cleaned test file

---
+++ README.txt
@@ -0,0 +1,8 @@
+Running the code:
+Preconditions, have a properly set up ocaml and opam environment and switch.
+
+run "make install", this should install all of the neccessary dependencies if your environment is properly well. If not, then follow suggestions given by the error messages.
+
+make test: will run our test cases by opening up a server and testing communicating with it.
+
+make repl: this will run the repl, see the design doc for usage descriptions.
--- test_file.ml
+++ test_file.ml
@@ -34,115 +34,6 @@ let _ = Thread.create Bs.mk_server_block
 let _ = Thread.create User.run_miner (uhi, chain_mux, chain_ref, blk_mux, blk_ref, blkchn, blkchn_mux, ipr, ipm)
       
 
-(* There are several paramteres that are currently hardcoded in the interest of rapid devlepment, they are listed below *)
-
-let max_hash = 100000
-
-(* is_valid_block is currently unimplemented, it returns true always currently for implementation purposes. Consequently, its test cases will fail *)
-
-(* Additionally, rsa signing with cryptokit is not yet implemented, we have our own signing system as a placehold *)
-
-(*
-   
-let st1 = Yojson.Basic.from_string "{
-  \"chain\":
-  [{
-      \"prev_hash\": 0,
-      \"time_stamp\":1524874389,
-      \"source\": \"2\",
-      \"dest\": \"personA\",
-      \"signature\": \"36\",
-      \"nonce\": 1,
-      \"amount\": 3.50,
-      \"complexity\": 15,
-      \"genesis\": false,
-      \"miner\": \"personB\",
-      \"n\":\"83487343\",
-      \"d\":\"837093280\",
-      \"msg\":\"6\"
-  }],
-  \"reward\": 250,
-  \"bits\": 64
-}
-"
-
-
-let () = print_endline "1"
-        
-let ch1 = Crypto.BlockChain.blockchain_of_json st1
-
-let () = print_endline "2"
-        
-let bj1 = Yojson.Basic.from_string "
-  {
-      \"prev_hash\": 0,
-      \"time_stamp\":1524874389,
-      \"source\": \"2\",
-      \"dest\": \"personA\",
-      \"signature\": \"36\",
-      \"nonce\": 1,
-      \"amount\": 3.50,
-      \"complexity\": 15,
-      \"genesis\": false,
-      \"miner\": \"67987\",
-      \"n\":\"83487343\",
-      \"d\":\"837093280\",
-      \"msg\":\"6\"
-  }
-"
-let bj2 = Yojson.Basic.from_string "
-  {
-      \"prev_hash\": 45,
-      \"time_stamp\":1524,
-      \"source\": \"2\",
-      \"dest\": \"personA\",
-      \"signature\": \"36\",
-      \"nonce\": 123,
-      \"amount\": 4.50,
-      \"complexity\": 15,
-      \"genesis\": false,
-      \"miner\": \"personB\",
-      \"n\":\"83487343\",
-      \"d\":\"837093280\",
-      \"msg\":\"6\"
-  }
-                                    "
-let b1 = block_of_json bj1
-let b2 = block_of_json bj2
-       
-
-
-let () = print_int (hash_block b1)
-let () = print_endline "" 
-let () = print_int (hash_block b2)
-let () = print_endline ""
-let () = print_endline (block_to_string b1)
-
-let m = Mutex.create ()
-
-let chn_queue = ref []
-
-let u = User.set_user "7" "23" "55"
-       
-let blkchn2 = User.mine m chn_queue
-(*b1 (ref []) m ch1*)
-let b2' = List.hd blkchn2.chain
-
-let () = print_endline (block_chain_to_string blkchn2)
-       
-let () = print_int (hash_block b1)
-
-let ub = User.make_transaction u "123" 7.60 blkchn2.chain
-        
-let t = check_block ub blkchn2
-
-let () = print_endline (block_to_string ub)
-let () = print_endline ub.msg
-
-let () = print_endline ""
-let () = if t then print_endline "true" else print_endline "false"
-
- *)
 
 let m = Mutex.create ()
              
@@ -157,7 +48,10 @@ let sample_chain = Yojson.Basic.from_file "sample_chain.txt" |> blockchain_of_js
 let insuf_mon = Yojson.Basic.from_file "insufficient_money.txt" |> blockchain_of_json
 let bad_trans = Yojson.Basic.from_file "bad_transaction.txt" |> blockchain_of_json
 
-let s1::s2::[] = sample_chain.chain
+let s1,s2 = match sample_chain.chain with
+  | a::b::[] -> a,b
+  | _ -> failwith "improper read in data"
+
 
 let () = print_int (hash_block s2)
 let () = print_endline "";print_int (hash_block s1)
